1ff3b704153d261ab38c2619aa7439f3ec4f6839
amir <shayeghia@gmail.com>
2018-08-22 12:41:57 -0400

    Initial version for Count III / before JavaFX

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+        //
     }
 }
